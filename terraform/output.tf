f7b7b2294e5970373e509c82c081dc1fb6a0222b
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-18 16:49:55 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,19 +1,11 @@
 output "ecr_repository_url" {
-  description = "ECR repository URL"
-  value       = aws_ecr_repository.strapi.repository_url
+  value = aws_ecr_repository.this.repository_url
 }
 
 output "ecs_cluster_name" {
-  description = "ECS cluster name"
-  value       = aws_ecs_cluster.this.name
+  value = aws_ecs_cluster.this.name
 }
 
 output "ecs_service_name" {
-  description = "ECS service name"
-  value       = aws_ecs_service.strapi.name
-}
-
-output "task_definition_arn" {
-  description = "Task definition ARN"
-  value       = aws_ecs_task_definition.strapi.arn
+  value = aws_ecs_service.this.name
 }
