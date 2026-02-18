ddd2f6c3d2fb6b46aaca08aee3da51076e647cfd
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-18 16:49:03 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,31 @@
-variable "AWS_REGION" {}
-variable "image_url" {}
-variable "execution_role_arn" {}
-variable "task_role_arn" {}
-variable "subnets" {
-  type = list(string)
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
-variable "security_groups" {
-  type = list(string)
+
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+  default     = "strapi"
+}
+
+variable "vpc_id" {
+  description = "VPC ID where ECS runs"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnet IDs for ECS EC2 instances"
+  type        = list(string)
+}
+
+variable "image_tag" {
+  description = "Docker image tag pushed from GitHub Actions"
+  type        = string
+}
+
+variable "ecs_instance_profile_name" {
+  description = "Existing EC2 instance profile name (IAM already created)"
+  type        = string
 }
